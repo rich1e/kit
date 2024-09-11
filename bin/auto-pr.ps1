1392256580252dbdee5f3829f9d14d8072e192a6
Rich1e Kung <yuqigong@outlook.com>
2024-09-11 14:47:02 +0800

    update apps list

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "rich1e/kit:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
